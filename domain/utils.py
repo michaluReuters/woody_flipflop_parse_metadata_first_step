925585accd411a6c78c3461a0301d5c9a941acdb
michalurbanski <michal.urbanski@thomsonreuters.com>
2023-02-16 11:12:49 +0100

    ADO_509284 [VIC] Metadata Import Step 1 - Trigger Metadata Import Function:
    - uncommented value

--- domain/utils.py
+++ domain/utils.py
@@ -8,8 +8,7 @@ def extract_data_from_sns_trigger(event):
     :param event: event triggering function
     :return: required data to be processed
     """
-    # event_dict = json.loads(event["Records"][0]["Sns"]["Message"]) #UNCOMENT AFTER TESTS
-    event_dict = event["Records"][0]["Sns"]["Message"]
+    event_dict = json.loads(event["Records"][0]["Sns"]["Message"])
 
     data = {}
     file_name_sns = event_dict["body"]["metadata"].get("name")
